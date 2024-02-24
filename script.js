f39ba7629ce370fb15115200e4a9dcf626d1925c
igeon22 <griffley2080@gmail.com>
2024-02-24 16:01:04 -0500

    animation

--- script.js
+++ script.js
@@ -27,3 +27,21 @@ window.addEventListener("scroll", function() {
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Pour gérer le scroll vers le haut lorsqu'on est en haut de la page
 }, false);
 
+function reveal() {
+    var reveals = document.querySelectorAll(".features");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
