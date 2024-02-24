72d980994152bd466468ef7e46fb4f595467cc60
igeon22 <griffley2080@gmail.com>
2024-02-24 14:28:24 -0500

    header scroll

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ sideContainer.addEventListener("click",()=>{
     lastBtn.classList.toggle("em")
     document.querySelector("nav").classList.toggle("hd")
 });
+
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  let currentScroll =  document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scroll vers le bas
+    document.querySelector("header").classList.add("hidden");
+  } else {
+    // Scroll vers le haut
+    document.querySelector("header").classList.remove("hidden");
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Pour gérer le scroll vers le haut lorsqu'on est en haut de la page
+}, false);
+
